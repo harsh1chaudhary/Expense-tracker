95bc81e92b1242ad6c8007ed3b3bba59b8b0fa7b
Harsh chaudhary <harshchaudhary271@gmail.com>
2025-06-30 22:45:35 +0530

    updated js #now able to add work , and see the tables and there elements only , #now new creation table till now

--- static/script.js
+++ static/script.js
@@ -1,13 +1,114 @@
 
+const collection_contain=document.getElementById('collection-container')
 
 
 
-async function showork() {
+
+
+
+function createtable(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+function showcasse(work){
+    const workdiv=document.createElement('div');
+    const details=document.createElement('details');
+    const summary=document.createElement('summary');
+    summary.textContent=work["workname"];
+    details.appendChild(summary)
+    try{
+
+    for (let i of work["table"]){
+        console.log(i);
+        const table=document.createElement('table');
+        const thead=document.createElement('thead');
+        const tbody=document.createElement('tbody');
+        const tr=document.createElement('tr');
+        const tdr=document.createElement('tr');
+      
+        for (let row in i){
+            const th=document.createElement('th');
+            console.log('row=',row);
+            th.textContent=row;
+            tr.append(th)
+            
+           
+        }
+        
+        
+      
+       
+      
+        for (let row in i){
+            console.log(i[row])
+            const td=document.createElement('td');
+            td.textContent=i[row];
+            tdr.appendChild(td);
+            
+
+
+        }
+        thead.append(tr)
+        tbody.appendChild(tdr);
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        details.appendChild(table);
+        
+    }
+
+
+    
+
+
+  
+
+
+    }
+    catch(erorr){
+        console.log("error",erorr)
+    }
+
+
+
+
+    workdiv.className='workdiv';
+    workdiv.appendChild(details)
+    collection_contain.appendChild(workdiv)
+
+
+}
+
+
+async function showork(worktittle=0) {
     try{
         const response=await fetch('/workdetails')
         const result=await response.json();
-        console.log('result',result[0])
+        console.log('result',(result));
+        if (worktittle==0){
+        for (let i in result){
+            showcasse(result[i])
+        }
     }
+       else {
+    const matched = result.find(item => item.workname === worktittle);
+    if (matched) {
+        showcasse(matched);
+    } else {
+        console.log('Work not found for title:', worktittle);
+    }
+}
+            
+
+        }
     catch (error){
         console.log('erorr',error);
     }
@@ -33,13 +134,13 @@ async function addwork(){
         const response=await fetch('/addwork',
             {method:'POST',        headers: {
             'Content-Type': 'application/json'
-        },body:JSON.stringify({workname:worktittlevalue})})
+        },body:JSON.stringify({workname:worktittlevalue,table:[]})})
         const result =await response.json();
         console.log("task",result)
     }catch (error){
         console.log('error',error)
     }
-    showork();
+    showork(worktittlevalue);
     
 
 
